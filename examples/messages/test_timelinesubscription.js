6b548c38daa7756499e48db873d6da6093f3ec7a
Rajiv Ramdhany <rajiv.ramdhany@bbc.co.uk>
2020-01-27 13:53:52 +0000

    Updates unit test for timelinesubscription

--- examples/messages/test_timelinesubscription.js
+++ examples/messages/test_timelinesubscription.js
@@ -1,7 +1,8 @@
 
-var MessageFactory = require("../../build/lib/MessageFactory");
+var MessageFactory = require("../../src/common/message/Message");
 var TimelineInfo = require("../../src/common/message/TimelineInfo");
 var Timestamp = require("../../src/common/timeline/Timestamp");
+var Correlation = require("../../src/common/message/Correlation");
 var PresentationTimestamp = require("../../src/common/timeline/PresentationTimestamp");
 
 // ---------------------------------------------------------
@@ -43,38 +44,34 @@ console.log(MessageFactory.Message.deserialise(requestAsBytes));
 // ---------------------------------------------------------
 
 
-console.log("---------------------------------------------------------");
-console.log("Build a TimelineSubscriptionRESP message with no Presentation Timestamp");
-console.log("---------------------------------------------------------");
-
-
-let TLSubResp = new MessageFactory.TimelineSubscriptionRESP(parsedReq.sessionId, 0, channel, null ,  parsedReq.id, parsedReq.version);
-
-console.log(TLSubResp);
-
-let TLSubRespAsBytes = TLSubResp.serialise();
-console.log("TimelineSubscriptionRESP serialised to binary.");
-
-let parsedRESP = MessageFactory.TimelineSubscriptionRESP.deserialise(TLSubRespAsBytes);
-console.log("TimelineSubscriptionRESP deserialised from binary: ");
-console.log(parsedRESP);
-
-
-console.log(MessageFactory.Message.deserialise(TLSubRespAsBytes));
-
-
 
 console.log("---------------------------------------------------------");
 console.log("Build a TimelineSubscriptionRESP message with only Actual Presentation Timestamp");
 console.log("---------------------------------------------------------");
 
 let actualTS = new Timestamp(10000.001, 9999999999.001, 1 );
-
 let pts = new PresentationTimestamp(null, actualTS, null);
+
+let parentTimelineId = "urn:wallclock";
+let parentCorr = new Correlation(1000, 10000, 0.5, 0.0001);
+let lastTimestamp =  new Timestamp(10000.001, 9999999999.001, 1 );
 
 console.log("PTS: " + JSON.stringify(pts));
 
-let TLSubResp2 = new MessageFactory.TimelineSubscriptionRESP(parsedReq.sessionId, 0, channel, pts ,parsedReq.id, parsedReq.version);
+const timeline = new TimelineInfo("timeline1" 
+								, timelineType
+								, contentId
+								, 1000000000
+								, "Sessions/" + sessionId + "/timelines/timeline1/"
+								, "device123"
+								, "phone"
+								, parentTimelineId
+								, parentCorr
+								,lastTimestamp);
+
+
+
+let TLSubResp2 = new MessageFactory.TimelineSubscriptionRESP(parsedReq.sessionId, 0, timeline ,parsedReq.id, parsedReq.version);
 
 console.log(TLSubResp2);
 
@@ -86,26 +83,26 @@ console.log("TimelineSubscriptionRESP deserialised from binary: ");
 console.log(parsedRESP2);
 
 
-console.log("---------------------------------------------------------");
-console.log("Build a TimelineSubscriptionRESP message with Earliest, Actual, Latest Presentation Timestamps");
-console.log("---------------------------------------------------------");
+// console.log("---------------------------------------------------------");
+// console.log("Build a TimelineSubscriptionRESP message with Earliest, Actual, Latest Presentation Timestamps");
+// console.log("---------------------------------------------------------");
 
 
 
-let earliestTS = new Timestamp(9995.005, 9999999999.001, 2.1 );
-let latestTS = new Timestamp(10005.005, 9999999999.001, 2.0 );
+// let earliestTS = new Timestamp(9995.005, 9999999999.001, 2.1 );
+// let latestTS = new Timestamp(10005.005, 9999999999.001, 2.0 );
 
-let pts3 = new PresentationTimestamp(earliestTS, actualTS, latestTS);
+// let pts3 = new PresentationTimestamp(earliestTS, actualTS, latestTS);
 
-console.log("PTS3: " + JSON.stringify(pts3));
+// console.log("PTS3: " + JSON.stringify(pts3));
 
-let TLSubResp3 = new MessageFactory.TimelineSubscriptionRESP(parsedReq.sessionId, 0, channel, pts3 ,parsedReq.id, parsedReq.version);
+// let TLSubResp3 = new MessageFactory.TimelineSubscriptionRESP(parsedReq.sessionId, 0, channel, pts3 ,parsedReq.id, parsedReq.version);
 
-console.log(TLSubResp3);
+// console.log(TLSubResp3);
 
-let TLSubRespAsBytes3 = TLSubResp3.serialise();
-console.log("TimelineSubscriptionRESP serialised to binary.");
+// let TLSubRespAsBytes3 = TLSubResp3.serialise();
+// console.log("TimelineSubscriptionRESP serialised to binary.");
 
-let parsedRESP3 = MessageFactory.TimelineSubscriptionRESP.deserialise(TLSubRespAsBytes3);
-console.log("TimelineSubscriptionRESP deserialised from binary: ");
-console.log(parsedRESP3);
+// let parsedRESP3 = MessageFactory.TimelineSubscriptionRESP.deserialise(TLSubRespAsBytes3);
+// console.log("TimelineSubscriptionRESP deserialised from binary: ");
+// console.log(parsedRESP3);

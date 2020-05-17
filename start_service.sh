a9096a03dcd98154bc6d9cb7adb534c03d07621b
Rajiv Ramdhany <rajiv.ramdhany@bbc.co.uk>
2020-05-17 14:09:36 +0100

    Updates STATS_WRITE_FLAG and  INFLUX_URL values

--- start_service.sh
+++ start_service.sh
@@ -65,8 +65,8 @@ myArray=($addresses)
 echo "calling docker-compose up"
 # grunt build_lib
 export INSTANCE_NAME=testcloudsync
-export STATS_WRITE_FLAG=ON
-export INFLUX_URL=http://localhost:9999 
+export STATS_WRITE_FLAG=OFF
+export INFLUX_URL=http://35.179.17.39:9999 
 export INFLUX_TOKEN=386kJJ-6RiCpFs9oTKqZXFH_7GmsE7Nq-vVAgtMEFW2q2GcygByARsgvyMrrD6rwGdJJEIux1gC4C4FcZmvcoA==
 export INFLUX_TOKEN_TWO=qUmv08RH5vPu__8ub41F_EqnXjdB1ajNdtPrqHnZHYWC58-onNFXpbvkFqLVRu6WkCppL0pt-3ufhaZJ6gYXjg==
 INSTANCE_NAME=testcloudsync docker-compose up --scale synccontroller=2

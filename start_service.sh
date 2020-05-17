2ecd2d06b2e8ac0dfae90398314c29eced7deb8e
Rajiv Ramdhany <rajiv.ramdhany@bbc.co.uk>
2020-05-17 16:01:43 +0100

    Adds sudo

--- start_service.sh
+++ start_service.sh
@@ -69,5 +69,5 @@ export STATS_WRITE_FLAG=ON
 export INFLUX_URL=http://localhost:9999 
 export INFLUX_TOKEN=386kJJ-6RiCpFs9oTKqZXFH_7GmsE7Nq-vVAgtMEFW2q2GcygByARsgvyMrrD6rwGdJJEIux1gC4C4FcZmvcoA==
 export INFLUX_TOKEN_TWO=qUmv08RH5vPu__8ub41F_EqnXjdB1ajNdtPrqHnZHYWC58-onNFXpbvkFqLVRu6WkCppL0pt-3ufhaZJ6gYXjg==
-INSTANCE_NAME=testcloudsync docker-compose up --scale synccontroller=2
+sudo docker-compose up --scale synccontroller=2
 

56d7b9602b76412f386ff648cb387e611ca856b4
Ubuntu <ubuntu@ip-172-31-3-137.eu-west-2.compute.internal>
2020-05-18 12:52:24 +0000

    Updates InfluxDB config params

--- src/statscontroller/src/env.js
+++ src/statscontroller/src/env.js
@@ -4,14 +4,14 @@ const url = process.env['INFLUX_URL'] || 'http://35.179.17.39:9999'
 /** InfluxDB authorization token */
 const token = process.env['INFLUX_TOKEN'] || '386kJJ-6RiCpFs9oTKqZXFH_7GmsE7Nq-vVAgtMEFW2q2GcygByARsgvyMrrD6rwGdJJEIux1gC4C4FcZmvcoA=='
 /** Organization within InfluxDB URL  */
-const org = process.env['INFLUXDB_ORG'] || '05af8ed8b05d4000'
+const org = process.env['INFLUXDB_ORG'] || 'BBC'
 /**InfluxDB bucket used in examples  */
 const bucket = 'CloudSync'
 // ONLY onboarding example
 /**InfluxDB user  */
-const username = 'rajivr'
+const username = 'bbctogether'
 /**InfluxDB password  */
-const password = 'paem2See'
+const password = 'w4tchp4rty'
 
 module.exports = {
 	url,
